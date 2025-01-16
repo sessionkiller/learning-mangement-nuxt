c69ca1d2f667c2fdf8d4c96d7e6c1cf2ad5a46d6
sessionkiller <najib.doukkani@gmail.com>
2025-01-16 14:14:18 +0100

    payment completion page

--- components/checkout/CompletionPage.vue
+++ components/checkout/CompletionPage.vue
@@ -1,3 +1,25 @@
-<script setup></script>
-
-<template>Competion</template>
+<template>
+  <div class="completion">
+    <div class="completion__content">
+      <div class="completion__icon">
+        <IconCheck class="w-16 h-16" />
+      </div>
+      <h1 class="completion__title">COMPLETED</h1>
+      <p class="completion__message">
+        🎉 You have made a course purchase successfully! 🎉
+      </p>
+    </div>
+    <div class="completion__support">
+      <p>
+        Need help? Contact our{{ " " }}
+        <PButton variant="link" asChild >
+          <a href="mailto:support@example.com" class="p-0 m-0 text-primary-700">customer support</a>
+        </PButton>
+        .
+      </p>
+    </div>
+    <div class="completion__action">
+      <NuxtLink to="user/courses"> Go to Courses </NuxtLink>
+    </div>
+  </div>
+</template>
